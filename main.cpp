1207b29b20c0fd8f12824f122063a331fdfae99b
James <lim.en.hui.james@gmail.com>
2017-04-10 20:22:36 +0800

    Added the main class file

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+/**
+ * The main method of the program.
+ * @param  argc Argument count
+ * @param  argv Arguments
+ * @return      An integer, 0 or otherwise representing successful execution or otherwise
+ */
+int main(int argc, char** argv)
+{
+  
+}
