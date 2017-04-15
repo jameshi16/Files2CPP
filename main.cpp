5f3dc07e6538b396237b2328d13f3308d5eac9ef
James <lim.en.hui.james@gmail.com>
2017-04-15 08:07:25 +0800

    Added the main.cpp that is used for testing

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 
+//Self-includes
+#include "Commands.h"
+#include "commandUtils.h"
+#include "fileUtils.h"
+
 /**
  * The main method of the program.
  * @param  argc Argument count
@@ -8,5 +13,17 @@
  */
 int main(int argc, char** argv)
 {
-  
+  /* Variable declaration */
+  typedef std::map<std::string, CommandPack<CommandsShare>> StoreOfCommands;
+  CommandsShare cs; //"N"
+  StoreOfCommands commandStore = {};
+
+  /* Emplace functions into map */
+  commandStore.emplace(std::make_pair("-t", CommandPack<CommandsShare>{Commands::test}));
+
+  /* Processes the argv array */
+  CommandUtilities::processArgv(argv, argc, commandStore, &cs);
+
+  /* Returns */
+  return 0;
 }
